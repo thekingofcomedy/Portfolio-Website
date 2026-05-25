d506099fcbce48b76cc2e3229f97f2f3024bed14
kiran <kiranneelr@gmail.com>
2026-05-25 16:56:00 +0200

    Github Path Fix

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,10 @@
 const navHTML = `<nav>
 
                 <ul>
-                <li><a href="/index.html">Home </a></li>
-                <li><a href="/HTML/about.html">About</a></li>
-                <li><a href="/HTML/projects.html">Projects</a></li>
-                <li><a href="/HTML/contact.html">Contact</a></li>
+                <li><a href="/Portfolio-Website/index.html">Home </a></li>
+                <li><a href="/Portfolio-Website/HTML/about.html">About</a></li>
+                <li><a href="/Portfolio-Website/HTML/projects.html">Projects</a></li>
+                <li><a href="/Portfolio-Website/HTML/contact.html">Contact</a></li>
 
             </ul>
 
@@ -25,7 +25,7 @@ const themeIcon = document.querySelector("#theme-icon");
 
 if (localStorage.getItem("darkMode") === "enabled") {
   document.body.classList.add("dark-mode");
-  themeIcon.src = "/Images/dark mode symbol.png";
+  themeIcon.src = "/Portfolio-Website/Images/dark mode symbol.png";
 }
 
 themeToggle.addEventListener("click", () => {
@@ -33,10 +33,10 @@ themeToggle.addEventListener("click", () => {
 
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("darkMode", "enabled");
-    themeIcon.src = "/Images/dark mode symbol.png";
+    themeIcon.src = "/Portfolio-Website/Images/dark mode symbol.png";
   } else {
     localStorage.setItem("darkMode", "disabled");
-    themeIcon.src = "/Images/Light Mode Symbol.png";
+    themeIcon.src = "/Portfolio-Website/Images/Light Mode Symbol.png";
   }
 });
 
